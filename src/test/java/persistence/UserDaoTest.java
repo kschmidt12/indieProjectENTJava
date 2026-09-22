83faecbfa505fe9974cb9e5e087add5f6b0e058b
kschmidt12 <kmatuszak@madisoncollege.edu>
2026-09-22 14:18:56 -0500

    Update, insert and delete test working

--- src/test/java/persistence/UserDaoTest.java
+++ src/test/java/persistence/UserDaoTest.java
@@ -18,14 +18,45 @@ class UserDaoTest {
 
     @Test
     void update() {
+        userDao = new UserDao();
+        User retrievedUser = userDao.getById(1);
+        retrievedUser.setUserName("changedUsername");
+        userDao.update(retrievedUser);
+        User changedUser = userDao.getById(1);
+        assertEquals("changedUsername", changedUser.getUserName());
     }
 
     @Test
     void insert() {
+        userDao = new UserDao();
+
+        User user = new User();
+        user.setName("Harper Schmidt");
+        user.setEmail("harper@gmail.com");
+        user.setUserName("HarperSchmidt");
+        user.setPassword("ILoveChickenNuggets");
+        userDao.insert(user);
+
+        User newUser = userDao.getById(user.getId());
+
+        assertEquals("Harper Schmidt", newUser.getName());
     }
 
     @Test
     void delete() {
+        userDao = new UserDao();
+
+        User user = new User();
+        user.setName("Deleted User");
+        user.setEmail("deleted@gmail.com");
+        user.setUserName("deleted");
+        user.setPassword("ImGoingToBeDeleted");
+        userDao.insert(user);
+
+        User retrievedUser = userDao.getById(user.getId());
+        userDao.delete(retrievedUser);
+        User deletedUser = userDao.getById(user.getId());
+        assertNull(deletedUser);
     }
 
     @Test
